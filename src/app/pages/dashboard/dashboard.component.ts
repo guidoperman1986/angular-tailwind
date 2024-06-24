b6add5b5a6607f658a08e917c2fd59238ebe94c8
guidopermanEleva <guido.perman@leniolabs.com>
2024-06-24 14:06:38 -0300

    tailwind config done, routes and dashboard initial layout

---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DashboardComponent {}
