517cb88e9bbe87ec4b0cc170533a14e04a8573a3
gperman <gperman@powerplan.com>
2024-07-13 23:24:49 -0300

    spinner moved and carrousel fixed

--- src/app/components/news-card/news-card.component.ts
+++ src/app/components/news-card/news-card.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { Article } from '../../interfaces/news.interface';
+import { SpinnerLoaderComponent } from '../spinner-loader/spinner-loader.component';
 
 @Component({
   selector: 'app-news-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SpinnerLoaderComponent],
   templateUrl: './news-card.component.html',
   styleUrl: './news-card.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
